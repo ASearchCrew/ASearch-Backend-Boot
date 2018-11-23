60940763ea2a7495606ca5da1f7180b6e846f0d6
chRyu <ggrkdehrud@gmail.com>
2018-11-23 09:17:40 +0900

    CLN:test

--- src/main/java/com/asearch/logvisualization/LogvisualizationApplication.java
+++ src/main/java/com/asearch/logvisualization/LogvisualizationApplication.java
@@ -8,6 +8,6 @@ public class LogvisualizationApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(LogvisualizationApplication.class, args);
-        //±â¿ì¿µ¾¾
+        //ê¸°ìš°ì˜
     }
 }
