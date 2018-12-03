c8365e84e5829ad4c377c9ccdaeedc8e14a27d92
KiWooyoung <ueong777@gmail.com>
2018-12-04 08:42:42 +0900

    ENH : made the basic settings of AlarmController

--- src/main/java/com/asearch/logvisualization/controller/AlarmController.java
+++ src/main/java/com/asearch/logvisualization/controller/AlarmController.java
@@ -1,4 +1,17 @@
 package com.asearch.logvisualization.controller;
 
+import io.swagger.annotations.Api;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+@Slf4j
+@Api(description = "알람", tags = {"alarm"})
+@RequestMapping(value = "/api/v1/alarm", produces = "application/json")
 public class AlarmController {
+
+
 }
