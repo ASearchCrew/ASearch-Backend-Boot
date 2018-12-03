801c7f649c9213614cc179d82071def49a08eec6
KiWooyoung <ueong777@gmail.com>
2018-12-03 17:46:53 +0900

    ENH : add swagger gradle

--- src/main/java/com/asearch/logvisualization/LogvisualizationApplication.java
+++ src/main/java/com/asearch/logvisualization/LogvisualizationApplication.java
@@ -1,13 +1,12 @@
 package com.asearch.logvisualization;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+        import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LogvisualizationApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(LogvisualizationApplication.class, args);
-        //aaa
     }
 }
---
+++ src/main/java/com/asearch/logvisualization/config/SwaggerConfiguration.java
@@ -0,0 +1,13 @@
+package com.asearch.logvisualization.config;
+
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * API document를 위한 Swagger conf 설정
+ */
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfiguration {
+}
