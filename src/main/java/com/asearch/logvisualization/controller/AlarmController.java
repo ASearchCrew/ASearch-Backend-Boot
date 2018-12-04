c2e843fccf8b679590b8d958a69361e2ec8d5f57
KiWooyoung <ueong777@gmail.com>
2018-12-05 00:12:47 +0900

    ENH #3 : make - register alarm keyword api
    make controller, serviceImpl. etc..

--- src/main/java/com/asearch/logvisualization/controller/AlarmController.java
+++ src/main/java/com/asearch/logvisualization/controller/AlarmController.java
@@ -1,11 +1,18 @@
 package com.asearch.logvisualization.controller;
 
+import com.asearch.logvisualization.dto.AlarmKeywordDto;
 import com.asearch.logvisualization.service.AlarmService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
 
 @RestController
 @AllArgsConstructor
@@ -15,4 +22,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class AlarmController {
 
     private AlarmService alarmService;
+
+    @ApiOperation(value = "탐지 키워드 등록", notes = "탐지할 키워드를 등록한다.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "Created"),
+            @ApiResponse(code = 409, message = "Conflict")
+    })
+    @CrossOrigin
+    @PostMapping("/keyword")
+    public ResponseEntity registerAlarmKeyword(@RequestBody AlarmKeywordDto keyword) throws IOException {
+        return alarmService.registerAlarmKeyword(keyword) ?
+                new ResponseEntity(HttpStatus.CREATED) :
+                new ResponseEntity(HttpStatus.CONFLICT);
+    }
 }
