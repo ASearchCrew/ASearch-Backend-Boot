7c732cc85c9b34b3dc8c28e01613acaf84e17cbc
KiWooyoung <ueong777@gmail.com>
2018-12-05 01:17:34 +0900

    set log controller

--- src/main/java/com/asearch/logvisualization/controller/LogController.java
+++ src/main/java/com/asearch/logvisualization/controller/LogController.java
@@ -1,7 +1,17 @@
 package com.asearch.logvisualization.controller;
 
+import io.swagger.annotations.Api;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@AllArgsConstructor
+@Api(description = "로그", tags = {"log"})
+@Slf4j
+@RequestMapping(value = "/api/v1/log", produces = "application/json")
 public class LogController {
+
+
 }
