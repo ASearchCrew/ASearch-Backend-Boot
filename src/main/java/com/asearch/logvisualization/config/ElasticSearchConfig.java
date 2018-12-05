bea803046ae46668b27d2c7a45bdbcf765664f26
KiWooyoung <ueong777@gmail.com>
2018-12-05 09:41:30 +0900

    ENH : create config component api

--- src/main/java/com/asearch/logvisualization/config/ElasticSearchConfig.java
+++ src/main/java/com/asearch/logvisualization/config/ElasticSearchConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-lasticSearchConfig {
+public class ElasticSearchConfig {
 
     @Value("${elasticsearch.host}")
     private String elasticsearchHost;
