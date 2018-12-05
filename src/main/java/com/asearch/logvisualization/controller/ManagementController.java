ee6722ebcf39f6093ae380d801bf599a83ffea6d
KiWooyoung <ueong777@gmail.com>
2018-12-05 09:43:12 +0900

    ENH : create - modify filebeat config api

--- src/main/java/com/asearch/logvisualization/controller/ManagementController.java
+++ src/main/java/com/asearch/logvisualization/controller/ManagementController.java
@@ -1,9 +1,30 @@
 package com.asearch.logvisualization.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Api(description = "관리", tags = {"management"})
 @AllArgsConstructor
+@RestController
+@RequestMapping(value = "/api/v1/management", produces = "application/json")
 public class ManagementController {
+
+    @ApiOperation(value = "Filebeat Configuration 변경", notes = "Fillbeat Conf를 변경한다. // Todo 수정 필요.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Success"),
+            @ApiResponse(code = 404, message = "Not Found")
+    })
+    @CrossOrigin
+    @PutMapping("/filebeat/conf")
+    public String modifyFilebeatConfiguration() {
+
+        return "우영";
+    }
 }
