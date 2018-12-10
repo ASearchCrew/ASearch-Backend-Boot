70c4dbad5d692a2dad6bd76ce7c95800714de924
KiWooyoung <ueong777@gmail.com>
2018-12-11 07:36:20 +0900

    CLN #3 : service refactoring

--- src/main/java/com/asearch/logvisualization/service/AlarmServiceImpl.java
+++ src/main/java/com/asearch/logvisualization/service/AlarmServiceImpl.java
@@ -1,6 +1,9 @@
 package com.asearch.logvisualization.service;
 
+import com.asearch.logvisualization.dao.AlarmDaoImpl;
 import com.asearch.logvisualization.dto.AlarmKeywordDto;
+import com.asearch.logvisualization.exception.AlreadyExistsException;
+import com.asearch.logvisualization.exception.InternalServerErrorException;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import netscape.javascript.JSObject;
@@ -16,9 +19,11 @@ import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.rest.RestStatus;
 import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.SearchHits;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.json.JSONObject;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ServerErrorException;
 
 import java.io.IOException;
 import java.util.*;
@@ -29,29 +34,27 @@ import static com.asearch.logvisualization.util.Constant.KEYWORD_TYPE;
 @AllArgsConstructor
 @Service
 @Slf4j
-public class AlarmServiceImpl implements AlarmService {
+public class AlarmServiceImpl extends BaseServiceImpl implements AlarmService {
 
     private RestHighLevelClient client;
+    private AlarmDaoImpl alarmDao;
 
     @Override
-    public boolean registerAlarmKeyword(AlarmKeywordDto keyword) throws IOException {
+    public void registerAlarmKeyword(AlarmKeywordDto keyword) throws IOException {
 
-        boolean flag = false;
         SearchRequest searchRequest = buildSearchRequest(KEYWORD_INDEX, KEYWORD_TYPE);
-        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
-        searchSourceBuilder.query(QueryBuilders.matchQuery("keyword", keyword.getKeyword()));
-        searchSourceBuilder.query(QueryBuilders.matchQuery("host_ip", keyword.getKeyword()));
-        searchRequest.source(searchSourceBuilder);
-        SearchResponse response = client.search(searchRequest, RequestOptions.DEFAULT);
-        log.info(Arrays.toString(response.getHits().getHits()));
-
-        return false;
-//        if (response.getHits().getHits().length == 0) return makeKeyword(client, keyword.getKeyword());
-//        else {
-//            for (SearchHit searchHit : response.getHits().getHits())
-//                if (keyword.getKeyword().equals(searchHit.getSourceAsMap().get("keyword").toString())) flag = true;
-//            return !flag && makeKeyword(client, keyword.getKeyword());
-//        }
+        SearchSourceBuilder searchSourceBuilder = buildSearchSourceRequest();
+        SearchHit[] searchHits = alarmDao.getExistedKeyword(searchRequest, searchSourceBuilder, new String[]{"keyword", "host_ip"},
+                new String[]{keyword.getKeyword(), keyword.getHostIp()});
+        if (searchHits.length == 1) throw new AlreadyExistsException("Already exist");
+        else {
+            Map<String, Object> jsonMap = new HashMap<>();
+            jsonMap.put("keyword", keyword.getKeyword());
+            jsonMap.put("host_ip", keyword.getHostIp());
+            IndexRequest indexRequest = buildIndexRequest(KEYWORD_INDEX, KEYWORD_TYPE, jsonMap);
+            IndexResponse indexResponse = alarmDao.indexNewKeyword(indexRequest);
+            if (!indexResponse.getResult().toString().equals("CREATED")) throw new InternalServerErrorException("Not created");
+        }
     }
 
     @Override
@@ -99,33 +102,24 @@ public class AlarmServiceImpl implements AlarmService {
         return results;
     }
 
-    private SearchRequest buildSearchRequest(String index, String type) {
-
-        SearchRequest searchRequest = new SearchRequest();
-        searchRequest.indices(index);
-        searchRequest.types(type);
-
-        return searchRequest;
-    }
-
-    private boolean makeKeyword(RestHighLevelClient client, String keyword) {
-        Map<String, Object> jsonMap = new HashMap<>();
-        jsonMap.put("keyword", keyword);
-        jsonMap.put("status", "200");
-//            jsonMap.put("serverHost", ip);
-
-        IndexRequest request = new IndexRequest(
-                "mytest",
-                "keyword")
-                .source(jsonMap);
-        try {
-            IndexResponse indexResponse = client.index(request, RequestOptions.DEFAULT);
-            log.info(indexResponse.toString());
-        } catch (ElasticsearchException e) {
-            if (e.status() == RestStatus.CONFLICT) {}
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return true;
-    }
+//    private boolean makeKeyword(RestHighLevelClient client, String keyword) {
+//        Map<String, Object> jsonMap = new HashMap<>();
+//        jsonMap.put("keyword", keyword);
+//        jsonMap.put("status", "200");
+////            jsonMap.put("serverHost", ip);
+//
+//        IndexRequest request = new IndexRequest(
+//                "mytest",
+//                "keyword")
+//                .source(jsonMap);
+//        try {
+//            IndexResponse indexResponse = client.index(request, RequestOptions.DEFAULT);
+//            log.info(indexResponse.toString());
+//        } catch (ElasticsearchException e) {
+//            if (e.status() == RestStatus.CONFLICT) {}
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//        return true;
+//    }
 }
