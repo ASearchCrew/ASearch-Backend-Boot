6d58333a78330d4a8afb7380adeba04d67091ed7
KiWooyoung <ueong777@gmail.com>
2018-12-10 17:31:12 +0900

    ENH #25 : modify AlarmKeywordDto variable

--- src/main/java/com/asearch/logvisualization/dto/AlarmKeywordDto.java
+++ src/main/java/com/asearch/logvisualization/dto/AlarmKeywordDto.java
@@ -4,10 +4,14 @@ import lombok.Data;
 import lombok.ToString;
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.Date;
+
 @Data
 @ToString
 @Slf4j
 public class AlarmKeywordDto {
 
     private String keyword;
+    private String hostIp;
+    private Date lastOccurrenceTime;
 }
