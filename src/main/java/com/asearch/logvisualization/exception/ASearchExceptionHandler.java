77437a6c38c0bee2f6bf7f4db8d5d6bac89e5bc0
KiWooyoung <ueong777@gmail.com>
2018-12-11 09:46:39 +0900

    BUG : fix build error

--- src/main/java/com/asearch/logvisualization/exception/ASearchExceptionHandler.java
+++ src/main/java/com/asearch/logvisualization/exception/ASearchExceptionHandler.java
@@ -1,7 +1,6 @@
 package com.asearch.logvisualization.exception;
 
 import com.asearch.logvisualization.dto.ErrorModel;
-import javafx.util.Builder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
